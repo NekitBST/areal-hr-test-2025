c657a7e1c8735e7728313ccb620a396450cf0c3c
Никита <nikata.shatalov.04@mail.ru>
2025-04-16 11:42:59 +0300

    removed migrations launch when backend starts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,27 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appConfig } from './config/app.config';
-import { execSync } from 'child_process';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-async function runMigrations() {
-  try {
-    console.log('Запуск миграций...');
-    execSync('npm run migrate:up', {
-      stdio: 'inherit',
-      cwd: join(__dirname, '..')
-    });
-    console.log('Миграции успешно выполнены');
-  } catch (error) {
-    console.error('Ошибка при выполнении миграций:', error);
-    process.exit(1);
-  }
-}
-
 async function bootstrap() {
-  await runMigrations();
-  
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.setGlobalPrefix('api');
