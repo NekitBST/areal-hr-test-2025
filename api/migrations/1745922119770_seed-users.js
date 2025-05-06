1d7a354bf4dde0d69380308b682d13d03efca35b
Никита <nikata.shatalov.04@mail.ru>
2025-05-06 23:22:54 +0300

    fixed user migration conflict (added user existence check before inserting into database during user migration)

--- api/migrations/1745922119770_seed-users.js
+++ api/migrations/1745922119770_seed-users.js
@@ -31,18 +31,25 @@ exports.up = async (pgm) => {
   ];
 
   for (const user of users) {
-    const password_hash = await argon2.hash(user.password, {
-      type: argon2.argon2id,
-      memoryCost: 65536,
-      timeCost: 3,
-      parallelism: 4
-    });
-
-    await pgm.db.query(
-      'INSERT INTO users (last_name, first_name, middle_name, login, password_hash, role_id) ' +
-      'VALUES ($1, $2, $3, $4, $5, $6)',
-      [user.last_name, user.first_name, user.middle_name, user.login, password_hash, user.role_id]
+    const existingUser = await pgm.db.query(
+      'SELECT id FROM users WHERE login = $1',
+      [user.login]
     );
+
+    if (existingUser.rows.length === 0) {
+      const password_hash = await argon2.hash(user.password, {
+        type: argon2.argon2id,
+        memoryCost: 65536,
+        timeCost: 3,
+        parallelism: 4
+      });
+
+      await pgm.db.query(
+        'INSERT INTO users (last_name, first_name, middle_name, login, password_hash, role_id) ' +
+        'VALUES ($1, $2, $3, $4, $5, $6)',
+        [user.last_name, user.first_name, user.middle_name, user.login, password_hash, user.role_id]
+      );
+    }
   }
 };
 
